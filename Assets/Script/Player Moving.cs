c99899185361aa9d9ccec6731c5290ca54685064
moyin <moyin@OA-S141-06>
2025-02-06 15:27:17 -0500

    Get component for the coffee item

--- Assets/Script/Player Moving.cs
+++ Assets/Script/Player Moving.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class PlayerMoving : MonoBehaviour
 {
-    float steerSpeed= 0.01f;
-    float moveSpeed = 0.01f;
+    public GameObject coffee;
+    public GameObject chatBubblePos;
+    public float steerSpeed= 0.01f;
+    public float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        coffee<GetComponent>
+        coffee = transform.position;
+        
+        float activeDistance = transform.position - coffeePos;
+
+
+
+
+        if ()
+        {
+
+        }
+
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
-        //transform.Rotate(0, 0, -steerAmount);
         transform.Translate(steerAmount, moveAmount, 0);
     }
 }
